1c854f9c50e9a7971955d119b96c123bb4f991da
samuelnapasco <samuelamo55@outlook.com>
2023-11-30 02:20:16 -1100

    initial commit

---
+++ middleware/error-handler.js
@@ -0,0 +1,32 @@
+const { CustomAPIError } = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+const errorHandlerMiddleware = (err, req, res, next) => {
+
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'something went wrong, try again later'
+  }
+
+
+  //if (err instanceof CustomAPIError) {
+  //  return res.status(err.statusCode).json({ msg: err.message })
+  //}
+  if(err.name === 'ValidationErroe'){
+    customError.msg = Object.values(err.errors).map((items)=>items.message).join(',')
+    customError.statusCode = 400
+  }
+
+  if(err.code && err.code === 11000){
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
+    customError.statusCode = 400
+  }
+
+  if(err.name === 'CastError'){
+    customError.msg = `No item found with id ${err.value}`
+    customError.statusCode = 404
+  }
+  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(StatusCodes.statusCode).json({msg: customError.msg})
+}
+
+module.exports = errorHandlerMiddleware
